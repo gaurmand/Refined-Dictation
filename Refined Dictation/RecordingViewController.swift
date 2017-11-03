02d9cf2b5479cd0c14e2e2e1e38e04c7713dc229
Admin <admin@Admins-Mac.local>
2017-11-03 23:02:50 +0300

    Added launch screen. Search field dismisses keyboard when user presses enter.

--- Refined Dictation/RecordingViewController.swift
+++ Refined Dictation/RecordingViewController.swift
@@ -8,12 +8,13 @@
 
 import UIKit
 
-class RecordingViewController: UIViewController {
+class RecordingViewController: UIViewController, UITextFieldDelegate {
     @IBOutlet weak var RecordingButton: UIButton!
+    @IBOutlet weak var SearchField: UITextField!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        SearchField.delegate = self //sets search field delegate
         // Do any additional setup after loading the view.
     }
 
@@ -22,17 +23,28 @@ class RecordingViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-    @IBAction func ChangeRecordButtonImage(_ sender: Any) {
-        let StopRecording = UIImage(named: "stop")
-        RecordingButton.setImage(StopRecording, for: UIControlState.normal)
-    }
+
 
     
     // MARK: - Navigation
-
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     @IBAction func unwindToRecordingView(sender: UIStoryboardSegue) {
         
+    }
+    
+    //MARK: UITextFieldDelegate
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        // Hide the keyboard when user presses return
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    
+    //MARK: Actions
+    
+    @IBAction func ChangeRecordButtonImage(_ sender: Any) {
+        let StopRecording = UIImage(named: "stop")
+        RecordingButton.setImage(StopRecording, for: UIControlState.normal)
     }
  
 
